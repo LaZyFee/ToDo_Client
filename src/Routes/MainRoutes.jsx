b58e0b4b4a3c2c48e9e6fad2e67debac110ad3ec
Rabiul Hasan Rafee <rhr277@gmail.com>
2024-10-29 11:48:42 +0600

    Done

--- src/Routes/MainRoutes.jsx
+++ src/Routes/MainRoutes.jsx
@@ -3,7 +3,7 @@ import AuthLayout from "../Layouts/AuthLayout";
 import Signup from "../Pages/Authentication/Signup";
 import Login from "../Pages/Authentication/Login";
 import MainLayout from "../Layouts/MainLayout";
-import PrivateRoute from "../Utils/privateRoute";
+import PrivateRoute from "../Utils/PrivateRoute";
 import AllToDos from "../Pages/MainLayoutPages/allToDos";
 import ActiveTodos from "../Pages/MainLayoutPages/ActiveTodos";
 import CompleteTodos from "../Pages/MainLayoutPages/CompleteTodos";
--- src/Utils/PrivateRoute.jsx
+++ src/Utils/PrivateRoute.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../Store/AuthStore";
